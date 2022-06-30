8ecc704330383a599221e829451ca808dc1444cc
Tjalle Borgers <tjalle@borgersonline.nl>
2022-06-30 16:16:25 +0200

    added audio and finished game

--- Assets/Script/Enemies/Imp.cs
+++ Assets/Script/Enemies/Imp.cs
@@ -3,9 +3,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Imp
+public class Imp : Character
 {
-    private void Update()
+    [SerializeField] private AudioClip _audioClip;
+    [SerializeField] private AudioSource _audioSource;
+    public override void TakeDamage(float damage, Character source)
     {
+        if(source==this) return;
+        Debug.Log("OOF");
+        health -= damage;
+        _audioSource.PlayOneShot(_audioClip);
+        _audioSource.gameObject.transform.SetParent(GameManager.Instance.transform);
+        Debug.Log(_audioSource.isPlaying);
+        if (health <= 0)
+        {
+            GameManager.Score++;
+            Destroy(gameObject);
+        }
     }
 }
